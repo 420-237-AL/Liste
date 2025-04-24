25e66d1bc61e246c2f975e883c3e89988642076e
David G <david.giasson@claurendeau.qc.ca>
2025-04-23 22:27:56 -0400

    Ajout de commentaires dans la classe File.

--- src/File.java
+++ src/File.java
@@ -1,15 +1,23 @@
 public class File <ParamType> {
     Liste<ParamType> liste;
 
-    public File() {
+    public File() { // Une "file" (ou Queue en anglais) est une structure adaptée d'une liste (typiquement).
         liste = new Liste<>();
     }
 
-    public void ajouter(ParamType element) {
+    public int getNbElements() {
+        return liste.getNbElements();
+    }
+
+    public boolean estVide() {
+        return liste.estVide();
+    }
+
+    public void ajouter(ParamType element) { // On ne peut ajouter des données qu'à la fin de la file.
         liste.ajouter(element);
     }
 
-    public boolean retirer(int index) {
+    public boolean retirer(int index) { // Et on retire toujours le premier élément de la file (le plus ancien).
         return liste.retirer(0);
     }
 }
